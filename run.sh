ae187f3fb70420cb2f70ace2a04fac2fe136b90b
Christian Niehoff <christian.niehoff@dwpbank.de>
2019-07-02 15:43:16 +0200

    hello terragrunt

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt-get update
 sudo apt-get -y install ansible
 
-ansible-playbook plays/playbook.yml -i hosts.ini --ask-sudo-pass
+ansible-playbook plays/playbook.yml -i hosts.ini
