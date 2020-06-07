420ed8b1d4c3540daf55518323a6aed77e66ce75
Christian Niehoff <christian.niehoff@dwpbank.de>
2020-06-07 11:27:06 +0200

    add hyper js terminal

---
+++ roles/common/files/.hyper.js
@@ -0,0 +1,39 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
+module.exports = {
+  config: {
+    // choose either `'stable'` for receiving highly polished,
+    // or `'canary'` for less polished but more frequent updates
+    updateChannel: 'stable',
+
+    shell: '/usr/bin/zsh',
+    
+    fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',    
+	  
+    // for environment variables
+    env: {},
+
+    // set to `false` for no bell
+    bell: 'SOUND',
+
+    // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
+    copyOnSelect: true,
+
+    // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
+    defaultSSHApp: true,
+
+
+    // Whether to use the WebGL renderer. Set it to false to use canvas-based
+    // rendering (slower, but supports transparent backgrounds)
+    webGLRenderer: false,
+
+    // for advanced config flags please refer to https://hyper.is/#cfg
+  },
+
+  plugins: [
+	'hyper-one-dark', 
+  	'hyper-font-ligatures'
+  ],
+};
